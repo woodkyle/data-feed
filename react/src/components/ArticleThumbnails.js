4f3705c2ba5776f6ee0fcdce94fd07b7fe232150
Kyle Wood <woodk53@gmail.com>
2016-10-24 15:46:36 -0400

    Refactored components into stateless

--- react/src/components/ArticleThumbnails.js
+++ react/src/components/ArticleThumbnails.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, Row, Col, Thumbnail } from 'react-bootstrap';
 
-export default class ArticleThumbnails extends Component {
-  constructor(props){
-    super(props);
-  }
+const ArticleThumbnails = props => {
 
-  render(){
-    let articles = this.props.articleArray.map(article => {
-      return (
-        <Thumbnail>
-          <h3>{article.title}</h3>
-          <p>{article.description}</p>
-          <p>Author: {article.author}</p>
-          <p><a href={article.url} target="_blank">Source</a></p>
-        </Thumbnail>
-      )
-    })
-    return(
-      <div>
-        {articles}
-      </div>
+  let articles = props.articleArray.map(article => {
+    return (
+      <Thumbnail>
+        <h3>{article.title}</h3>
+        <p>{article.description}</p>
+        <p>Author: {article.author}</p>
+        <p><a href={article.url} target="_blank">Source</a></p>
+      </Thumbnail>
     )
-  }
+  })
 
-}
+  return(
+    <div>
+      {articles}
+    </div>
+  );
+};
+
+export default ArticleThumbnails;
--- react/src/components/DropdownBar.js
+++ react/src/components/DropdownBar.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ButtonToolbar, ButtonGroup, DropdownButton, MenuItem, Popover, OverlayTrigger } from 'react-bootstrap';
 
-export default class DropdownBar extends Component {
+const DropdownBar = props => {
 
-  constructor(props){
-    super(props);
-  }
-
-  render() {
-    let dropMenus = this.props.categories.map( category => {
-      let menuItems = this.props.sourceArray.map( s => {
-        let popover = (
-          <Popover id="popover-hover" title={s.name}>
-            {s.description}
-          </Popover>
-        )
-        if (s.category === category) {
-          let handleClick = () => this.props.handleClick(s.apiKey)
-          return (
-            <OverlayTrigger trigger={['hover', 'focus']} placement="bottom" overlay={popover}>
-              <MenuItem onClick={handleClick}>{s.name}</MenuItem>
-            </OverlayTrigger>
-          )
-        }
-      })
-      return(
-        <DropdownButton title={category} id="bg-justified-dropdown">
-          {menuItems}
-        </DropdownButton>
+  let dropMenus = props.categories.map( category => {
+    let menuItems = props.sourceArray.map( s => {
+      let popover = (
+        <Popover id="popover-hover" title={s.name}>
+          {s.description}
+        </Popover>
       )
+      if (s.category === category) {
+        let handleClick = () => props.handleClick(s.apiKey)
+        return (
+          <OverlayTrigger trigger={['hover', 'focus']} placement="bottom" overlay={popover}>
+            <MenuItem onClick={handleClick}>{s.name}</MenuItem>
+          </OverlayTrigger>
+        )
+      }
     })
-    return (
-      <div>
-        <ButtonToolbar>
-          <ButtonGroup justified>
-            {dropMenus}
-          </ButtonGroup>
-        </ButtonToolbar>
-      </div>
+    return(
+      <DropdownButton title={category} id="bg-justified-dropdown">
+        {menuItems}
+      </DropdownButton>
     )
-  }
-}
+  })
+  return (
+    <div>
+      <ButtonToolbar>
+        <ButtonGroup justified>
+          {dropMenus}
+        </ButtonGroup>
+      </ButtonToolbar>
+    </div>
+  );
+};
+
+export default DropdownBar;
