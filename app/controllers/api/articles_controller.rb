52bb401ad9d7d72dcf881ccd1b2b1c9b04f20a3f
Kyle Wood <woodk53@gmail.com>
2016-10-25 15:49:44 -0400

    Added trending sources

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,24 +1,24 @@
 class Api::ArticlesController < ApiController
 
-  def index
-    @sources = Source.all
-    respond_to do |format|
-      format.json do
-        render json: @sources
-      end
-    end
-  end
-
   def create
-    @source = params[:sourceTitle]
-    @key = ENV["NEWS_API_KEY"]
-    @url = "https://newsapi.org/v1/articles?source=" + @source + "&apiKey=" + "#{@key}"
-    @articles = HTTParty.get(@url)["articles"]
+    @article = Article.create(article_params)
+    @articles = Article.all
+    if @articles.length > 30
+      Article.first.destroy
+      @articles = Article.all
+    end
+    @articles = @articles
     respond_to do |format|
       format.json do
         render json: @articles
       end
     end
+  end
+
+  protected
+
+  def article_params
+    params.permit(:author, :title, :description, :url, :urlToImage, :publishedAt)
   end
 
 end
--- react/src/components/PinnedArticles.js
+++ react/src/components/PinnedArticles.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Thumbnail, Grid, Row, Col, Button } from 'react-bootstrap';
 
-const ArticleThumbnails = props => {
+const PinnedArticles = props => {
 
-  let articles = props.articleArray.map(article => {
+  let articles = props.pinnedArticles.map(article => {
     if (article.urlToImage == null){
       article.urlToImage = props.currentSourceLogoUrl;
     }
     if (article.author == null){
       article.author = "Not Available";
     }
-    let handlePin = () => props.handlePin(article)
     return (
       <Thumbnail>
         <Grid>
@@ -20,7 +19,6 @@ const ArticleThumbnails = props => {
               <p>{article.description}</p>
               <p>Author: {article.author}</p>
               <Button bsStyle="primary" bsSize="small" href={article.url} target="_blank">Source Article</Button>
-              <Button bsStyle="primary" bsSize="small" onClick={handlePin}>Like!</Button>
             </Col>
             <Col xs={6} md={4}>
               <img src={article.urlToImage} height={150} width={250} /><br />
@@ -39,4 +37,4 @@ const ArticleThumbnails = props => {
   );
 };
 
-export default ArticleThumbnails;
+export default PinnedArticles;
